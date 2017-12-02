ea12c056d0301a4a64160943479d5436d7dba7e1
TeanLee <ltt598625763@163.com>
2017-12-02 23:49:46 +0800

    绑定了增加或者减少购买数量的函数，踩坑：为每一件商品添加了数量，方便后面的购物车结算

--- pages/shopping/shopping.js
+++ pages/shopping/shopping.js
@@ -1,3 +1,4 @@
+const app = getApp();
 // pages/shopping/shopping.js
 Page({
 
@@ -5,14 +6,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    goodsList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setData({
+      goodsList: app.globalData.cardList
+    })
+  },
+  addCount:function (e) {
+    var that = this;
+    console.log(e);
+    const goodId = e.currentTarget.id;
+    console.log(that.data.goodsList[goodId]);
+    that.data.goodsList[goodId].count++;
+    console.log(that.data.goodsList[goodId]);
+  },
+  reduceCount: function(e) {
+    var that = this;
+    const goodId = e.currentTarget.id;
+    // console.log(that.data.goodsList[goodId]);
+    if(that.data.goodsList[goodId].count <= 0) {
+      that.data.goodsList[goodId].count = 1;
+      wx.showModal({
+        title: '数量小于1',
+        content: '不允许操作',
+        duration: 2000
+      })
+    } else {
+      that.data.goodsList[goodId].count--;
+    }
+    // console.log(that.data.goodsList[goodId]);
   },
 
   /**
@@ -26,7 +53,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    console.log(this.data.goodsList);
   },
 
   /**
