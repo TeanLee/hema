d36ceb276e6b72d466311ba72bb0da72e5a9cc21
TeanLee <ltt598625763@163.com>
2017-12-09 14:32:15 +0800

    对点击加入购物车的商品进行判断，如果购物车中不存在该商品就加入购物车，否则直接增加该商品的数量。

--- pages/goodsList/goodsList.js
+++ pages/goodsList/goodsList.js
@@ -24,7 +24,25 @@ Page({
     // console.log(this.data.goods);
     const good = this.data.goods[e.currentTarget.id]; // 根据index，判断用户点击了哪个商品加入购物车
     const cart = app.globalData.cardList; // 获取购物车列表
-    cart.push(good); // 用户选择商品加入购物车后，将该商品加入购物车列表
+    // 设置一个标记，判断用户想加入购物车的商品是否已经存在购物车了
+    // some 是es6新增的方法，用于遍历整个数组，如果数组中存在一个及以上元素，就返回true
+    var flag = cart.some(() => {
+      return this.data.goods[e.currentTarget.id];
+    })
+    // console.log(flag);
+    // 如果购物车中没有该元素，就将该商品加入购物车，否则就将该商品的购买数量加一
+    if(!flag) {
+      cart.push(good); // 用户选择商品加入购物车后，将该商品加入购物车列表
+      wx.showToast({
+        title: '商品已加入购物车',
+        icon: 'success',
+        duration: 2000
+      })
+    } else {
+      this.data.goods[e.currentTarget.id].count ++;
+      // console.log(this.data.goods[e.currentTarget.id]);
+    }
+    
     // console.log(e.currentTarget.id);
     // console.log(cart);
   },
